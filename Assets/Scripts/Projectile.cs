06b651b749375a32ba40ab52bc5a749bcfbb35ce
alextagun <alextagun@gmail.com>
2021-05-22 01:56:39 +0300

    add damage logic

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -13,4 +13,14 @@ public class Projectile : MonoBehaviour {
     private void Awake() {
         _rb.velocity = new Vector2(0, _speed);
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Collision");
+        var tower = other.GetComponent<Tower>();
+        if(tower == null) return;
+        if (tower.IsEnemy) {
+            tower.GetDamage(_damage);
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tower : MonoBehaviour {
+    [SerializeField] private float _maxHP;
+    [SerializeField] private bool _isEnemy;
+
+    public bool IsEnemy => _isEnemy;
+
+
+    private float _curHP;
+    private void Awake() {
+        _curHP = _maxHP;
+    }
+
+    public void GetDamage(float value) {
+        _curHP -= value;
+        
+        if(_curHP <= 0) Destroy(gameObject);
+    }
+}
