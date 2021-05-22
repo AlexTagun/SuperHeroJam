a3c7331732dd97a7b3b332b4978c13d9d3f1ab16
alextagun <alextagun@gmail.com>
2021-05-22 16:06:55 +0300

    add hp bars

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,18 +7,25 @@ using UnityEngine.UI;
 public class Tower : MonoBehaviour {
     [SerializeField] private float _maxHP;
     [SerializeField] private bool _isEnemy;
+    [SerializeField] private Image hpBar;
 
     public bool IsEnemy => _isEnemy;
 
-
     private float _curHP;
+
     private void Awake() {
         _curHP = _maxHP;
+        UpdateHpBar();
     }
 
     public void GetDamage(float value) {
         _curHP -= value;
-        
-        if(_curHP <= 0) Destroy(gameObject);
+        UpdateHpBar();
+        if (_curHP <= 0) Destroy(gameObject);
+    }
+
+    private void UpdateHpBar() {
+        if(hpBar == null) return;
+        hpBar.fillAmount = _curHP / _maxHP;
     }
 }
