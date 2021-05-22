c2d7064ea71c7b6bd4716d12597f77a7fb281174
alextagun <alextagun@gmail.com>
2021-05-22 19:35:38 +0300

    add stars + mana calculation

--- Assets/Scripts/UICard.cs
+++ Assets/Scripts/UICard.cs
@@ -8,19 +8,31 @@ using UnityEngine.UI;
 public enum ElementType {
     Fire,
     Water,
-    Earth,
-    Glue
+    Earth
+}
+
+public enum FormType {
+    Base,
+    Ball,
+    Wall,
+    Lance
 }
 
 public class UICard : MonoBehaviour {
     [SerializeField] private ElementType _element;
+    [SerializeField] private FormType _form;
     [SerializeField] private int _rank;
     public int EnergyCost;
     [SerializeField] private Projectile _projectilePrefab;
     [SerializeField] private TextMeshProUGUI _energyText;
+    [SerializeField] private GameObject[] _stars;
 
     private void Awake() {
+        EnergyCost = _rank + (_form == FormType.Base ? 0 : 1);
         _energyText.text = EnergyCost.ToString();
+        for (int i = 0; i < _rank; i++) {
+            _stars[i].SetActive(true);
+        }
     }
 
     public ElementType Element => _element;
