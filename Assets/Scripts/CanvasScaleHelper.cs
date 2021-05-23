35a15aceebea4d4a1b5505b2b70bdc17ec912eaf
alextagun <alextagun@gmail.com>
2021-05-23 21:33:21 +0300

    adapt screen

---
+++ Assets/Scripts/CanvasScaleHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class CanvasScaleHelper : MonoBehaviour
+{
+    private void Awake() {
+        var canvasScaler = GetComponent<CanvasScaler>();
+        var ratio = Screen.width / (float) Screen.height;
+        canvasScaler.matchWidthOrHeight = ratio > 0.5625 ? 1 : 0;
+    }
+}
